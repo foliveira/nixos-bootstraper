8f4fe7fd15ffc6edfb849fcd86599fd2befb1ec4
Fábio Oliveira <foliveira@users.noreply.github.com>
2023-11-30 01:23:18 +0000

    Update default.nix (#11)
    Added more config to Macbook
    
    Signed-off-by: Fábio Oliveira <foliveira@users.noreply.github.com>

--- builders/macbook-pro-x86_64/default.nix
+++ builders/macbook-pro-x86_64/default.nix
@@ -1,15 +1,22 @@
 { config, pkgs, ... }:
 {
   imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-plasma5.nix>
+    <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-plasma5.nix>
     <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix>
   ];
 
   nixpkgs.config.allowUnfree = true;
-
+  
+  time.timeZone = "Europe/Lisbon";
+  
   boot = {
     kernelModules = [ "wl" ];
     extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
     blacklistedKernelModules = [ "b43" "bcma" ];
-  }
+  };
+
+  hardware = {
+    enableAllFirmware = true;
+    cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
 }
